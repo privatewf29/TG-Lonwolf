38a1fa0bfc0b28a2f511ea8a56fc48fa42c952b7
nerawgg@gmail.com <nerawgg@gmail.com>
2021-04-08 06:19:45 +0700

    TG-Lonwolf

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -20,7 +20,7 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/W29F/TG-LoneUbot/TG-LoneUbot/varshelper.txt)"
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/W29F/TG-Lonwolf/master/varshelper.txt)"
     )
 
 
